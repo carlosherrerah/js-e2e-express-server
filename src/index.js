7a781ea46109cb67b1e6a5ec3001b0d57cf1791f
carlosherrerah <carlos.herrera@upa.edu.mx>
2024-11-08 14:52:13 -0600

    puerto

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const server = require('./server');
 // App Service on Windows: PORT
 // App Service on Linux: WEB_PORT
 const port = process.env.PORT || 3000;
-const hostname = '0.0.0.0';
+const hostname = '127.0.0.1';
 
 server.create()
     .then(app => {
